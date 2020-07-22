5f7a9f23c64178e52e46a7ccb5e088c1052d7f4e
reneweiser <rene@encoding.group>
2020-07-22 14:15:38 +0200

    Cleaned up java script

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const app = document.querySelector(".app");
-const boxes = [];
+const createBoxes = () => {
+  let boxes = [];
+  for (let i = 0; i <= 100; i++) {
+    let box = document.createElement("img");
+    box.className = "box";
+    box.setAttribute("src", "./images/logo.jpg");
+    setBoxSize(box, 200, 200);
+    boxes.push(box);
+    app.appendChild(box);
+  }
+
+  return boxes;
+};
 
 const setBoxSize = (image, x, y) =>
   (image.style = `
@@ -9,19 +20,7 @@ const setBoxSize = (image, x, y) =>
     border-width: ${0.04 * x}px;
   `);
 
-for (let i = 0; i <= 100; i++) {
-  let box = document.createElement("img");
-  box.className = "box";
-  box.setAttribute("src", "./images/logo.jpg");
-  setBoxSize(box, 200, 200);
-  boxes.push(box);
-  app.appendChild(box);
-}
-
-const slider = document.querySelector(".slider-input");
-const sliderValue = document.querySelector(".slider-value");
-
-const resizeBoxes = function (size) {
+const resizeBoxes = function (boxes, size) {
   sliderValue.innerText = `${size / 100}x`;
   let i = boxes.length;
   while (i--) {
@@ -29,15 +28,20 @@ const resizeBoxes = function (size) {
   }
 };
 
-slider.oninput = (event) => resizeBoxes(event.target.value);
-
-window.onload = () => resizeBoxes(500);
-
-const selection = document.querySelector("#layout");
-
 const onChangeLayout = (event) => {
-  console.log(event.target.value);
   app.className = `app ${event.target.value}`;
 };
 
-selection.addEventListener("change", onChangeLayout);
+const app = document.querySelector(".app");
+const sliderInput = document.querySelector(".slider-input");
+const sliderValue = document.querySelector(".slider-value");
+const layoutSelect = document.querySelector(".select-layout");
+const boxes = createBoxes();
+
+sliderInput.addEventListener("input", (event) =>
+  resizeBoxes(boxes, event.target.value)
+);
+
+window.addEventListener("load", () => resizeBoxes(boxes, 500));
+
+layoutSelect.addEventListener("change", onChangeLayout);
